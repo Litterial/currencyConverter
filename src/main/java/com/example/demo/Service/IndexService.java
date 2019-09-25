b131b9449ca0a8b5693fa54abb4b62c150332130
Litterial <yungice12@gmail.com>
2019-09-24 22:25:37 -0500

    can now view data from site

--- src/main/java/com/example/demo/Service/IndexService.java
+++ src/main/java/com/example/demo/Service/IndexService.java
@@ -2,6 +2,8 @@ package com.example.demo.Service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -10,12 +12,20 @@ public class IndexService {
 
     public void testHTTP (String targetURL)
     {
+        String inputLine;
         try {
             URL url = new URL(targetURL);
+            BufferedReader reader=new BufferedReader(new InputStreamReader(url.openStream()));
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             System.out.println("Printing Response Code");
             System.out.println(connection.getResponseCode());
+
+            while(reader.readLine() != null){
+                inputLine = reader.readLine();
+                System.out.println(inputLine);
+            }
+            reader.close();
         }
 
         catch (Exception e){
