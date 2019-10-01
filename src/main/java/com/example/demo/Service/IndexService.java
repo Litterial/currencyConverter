8437178aca815bc0fbb3535ded1a8964eac7e405
Litterial <yungice12@gmail.com>
2019-10-01 09:55:46 -0500

    using jsonarray dependency to parse json

--- src/main/java/com/example/demo/Service/IndexService.java
+++ src/main/java/com/example/demo/Service/IndexService.java
@@ -1,35 +1,52 @@
 package com.example.demo.Service;
 
+import org.json.JSONArray;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class IndexService {
 
     public void testHTTP (String targetURL)
     {
+        List<StringBuffer> convertArray;
+        //read all lines
         String inputLine;
+        //StringBuffer is used to append lines
+        StringBuffer content= new StringBuffer();
         try {
             URL url = new URL(targetURL);
+            // response from a url connection is an inputstream
             BufferedReader reader=new BufferedReader(new InputStreamReader(url.openStream()));
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             System.out.println("Printing Response Code");
             System.out.println(connection.getResponseCode());
 
-            while(reader.readLine() != null){
-                inputLine = reader.readLine();
-                System.out.println(inputLine);
+            while((inputLine = reader.readLine()) != null){
+                content.append(inputLine);
             }
             reader.close();
+            convertArray=new ArrayList<>();
+            convertArray.add(content);
+            System.out.println(content.toString());
+//            parseJson(content.toString());
         }
 
         catch (Exception e){
             System.out.println("No connection");
         }
+    }
+
+    public static void parseJson (String responseBody)
+    {
+        JSONArray jsonArray = new JSONArray(responseBody);
+        System.out.println(jsonArray);
     }
 }
